3e4129bfb729f7efc20f97bd575a74e4de17da0a
Julia Sanchis <jursanchis@gmail.com>
2024-02-28 18:05:42 +0100

    delete useless files

--- src/utilities/formarCurrency.ts
+++
@@ -1,9 +0,0 @@
-const CURRENCY_FORMATTER = new Intl.NumberFormat("en-ES", {
-  currency: "EUR",
-  style: "currency",
-  maximumSignificantDigits: 3,
-});
-
-export function formatCurrency(number: number) {
-  return CURRENCY_FORMATTER.format(number);
-}
